c0ae323f04ece76c952639dc6c3d231df1c91b93
mianmiantea2019 <sguo321@gatech.edu>
2023-10-08 00:56:39 -0400

    setup base response and error status code

---
+++ backend/share-api-common/src/main/java/com/shareapi/apicommon/common/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.shareapi.shareapicommon.common;
+
+public enum ErrorCode {
+
+    SUCCESS(0, "ok"),
+    PARAMS_ERROR(40000, "Request parameter error"),
+    NOT_LOGIN_ERROR(40100, "not logged in"),
+    NO_AUTH_ERROR(40101, "No Authority"),
+    NOT_FOUND_ERROR (40400, "Request data does not exist"),
+    FORBIDDEN_ERROR (40300, "Prohibited Access"),
+    SYSTEM_ERROR(50000, "System Internal Exception"),
+    OPERATION_ERROR (50001, "Operation failed");
+
+    private final int code;
+    private final String message;
+
+    ErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
---
+++ backend/share-api-common/src/main/java/com/shareapi/apicommon/common/IdRequest.java
@@ -0,0 +1,13 @@
+package com.shareapi.shareapicommon.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class IdRequest implements Serializable {
+
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
